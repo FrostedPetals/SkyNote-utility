af794da3cca022091b2bbf3eeb56549174ad5cbf
Avidha <haldaravidha94@gmail.com>
2025-11-01 14:48:10 +0530

    fixup of font-II

--- chrome_ext/src/components/Home.jsx
+++ chrome_ext/src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   const {theme}=useContext(Themecontext)
   return (
     <>
-    <div className={` text-4xl sm:text-5xl md:text-9xl font-bold tracking-wide ${theme==='light'?`
+    <div className={`text-6xl md:text-9xl font-bold tracking-wide ${theme==='light'?`
    text-gray-900
   [text-shadow:_0_0_8px_rgb(255_255_255_/_0.6),_0_0_16px_rgb(200_200_255_/_0.5)]`:`text-white
   [text-shadow:_0_0_12px_rgb(0_255_255_/_0.8),_0_0_28px_rgb(0_200_255_/_1)]` }`}>
